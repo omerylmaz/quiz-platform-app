1af5b8540483f1bdfa8cab3c57c7c2a04a4b081c
Ömer Yılmaz <omerisler20@gmail.com>
2025-01-13 00:51:49 +0300

    initial commit

---
+++ Backend/src/Modules/Users/Users.Application/ServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Users.Application;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
--- Backend/src/Modules/Users/Users.Endpoints/ModuleRegistration.cs
+++ Backend/src/Modules/Users/Users.Endpoints/ModuleRegistration.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Users.Application;
-using Users.Infrastructure;
-
-namespace Users.Endpoints;
-
-public static class ModuleRegistration
-{
-    public static IServiceCollection AddUsersModule(this IServiceCollection services)
-    {
-        return services
-            .AddApplicationServices()
-            .AddInfrastructureServices();
-    }
-}
+﻿// using Microsoft.Extensions.DependencyInjection;
+// using Users.Application;
+// using Users.Infrastructure;
+//
+// namespace Users.Endpoints;
+//
+// public static class ModuleRegistration
+// {
+//     public static IServiceCollection AddUsersModule(this IServiceCollection services)
+//     {
+//         return services
+//             .AddApplicationServices()
+//             .AddInfrastructureServices();
+//     }
+//
+//     public static IApplicationBuilder UseUsersModule(this IApplicationBuilder app)
+//     {
+//         
+//     }
+// }
