52168ca062de03e940405bd130100a534db2787a
Ömer Yılmaz <omerisler20@gmail.com>
2025-01-25 21:33:43 +0300

    validation pipeline behavior added

--- Backend/src/Common/Common.Application/ApplicationConfiguration.cs
+++ Backend/src/Common/Common.Application/ApplicationConfiguration.cs
@@ -17,6 +17,7 @@ public static class ApplicationConfiguration
 
             config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
             config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);
